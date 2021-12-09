88a3a4edddab242512304df659551ac475a16dc5
axkeyz <xahkun@gmail.com>
2021-12-09 20:46:26 +1300

    Add utility functions
    - MapifyStruct converts a struct to a map[string]interface{}

--- internal/utils/strings.go
+++ internal/utils/strings.go
@@ -1,5 +1,10 @@
 package utils
 
+import(
+	"regexp"
+	"encoding/json"
+)
+
 // HasNoEmptyParams returns true if none of the strings in the
 // params slice is empty.
 func HasNoEmptyParams(params []string) bool {
@@ -9,4 +14,20 @@ func HasNoEmptyParams(params []string) bool {
 		}
 	}
 	return true
+}
+
+// IsEmail returns true if the given string appears to be an email
+func IsEmail(email string) bool {
+    emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+    return emailRegex.MatchString(email)
+}
+
+// MapifyStruct converts a struct to a map[string]interface
+func MapifyStruct(s interface{}) map[string]interface{} {   
+    var inInterface map[string]interface{}
+
+    inrec, _ := json.Marshal(s)
+    json.Unmarshal(inrec, &inInterface)
+
+    return inInterface
 }
